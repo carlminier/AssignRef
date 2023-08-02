9cc6a6ac96f01ecbb5395130e20f9ff8dbc258eb
Carl Minier <carl.a.minier@gmail.com>
2023-08-01 22:39:36 -0700

    Update and rename TrainingVideos_SelectBy_ConferenceId_SeasonIdV2.sql to TrainingVideos_SelectBy_ConferenceId_SeasonIdV2.sql

---
+++ Videos_Backend/TrainingVideos_SelectBy_ConferenceId_SeasonIdV2.sql
@@ -0,0 +1,82 @@
+﻿USE [AssignRef]
+GO
+/****** Object:  StoredProcedure [dbo].[TrainingVideos_SelectBy_ConferenceId_SeasonIdV2]    Script Date: 7/3/2023 8:34:36 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author: Carl Minier
+-- Create date: 6/16/2023
+-- Description:[dbo].[TrainingVideos_SelectBy_ConferenceId_SeasonIdV2]
+--			    
+-- Code Reviewer: Jaustin Buenaventura
+-- MODIFIED BY: 
+-- MODIFIED DATE:
+-- Code Reviewer:
+-- Note:
+--
+-- =============================================
+
+ALTER proc [dbo].[TrainingVideos_SelectBy_ConferenceId_SeasonIdV2]
+				@ConferenceId int,
+				@SeasonId int,
+				@PageIndex int,
+				@PageSize int
+
+as
+/*
+		Declare 
+				@ConferenceId int = 3
+				,@SeasonId int = 49
+				,@PageIndex int = 0
+				,@PageSize int = 10
+
+		Execute 
+				[dbo].[TrainingVideos_SelectBy_ConferenceId_SeasonIdV2]
+				@ConferenceId
+				,@SeasonId
+				,@PageIndex
+				,@PageSize
+*/
+
+BEGIN
+
+	Declare @offset int = @PageIndex * @PageSize
+
+	SELECT tv.[Id]
+		  ,vc.[Id] as CategoryId
+		  ,vc.[Name] as CategoryName
+		  ,[Title]
+		  ,[Subject]
+		  ,s.[Id] as SeasonId
+		  ,s.[Name] as SeasonName
+		  ,s.[Year] as SeasonYear
+		  ,c.[Id] as ConferenceId
+		  ,c.[Logo] as ConferenceLogo
+		  ,c.[Name] as ConferenceName
+		  ,c.[Code] as ConferenceCode
+		  ,[MediaUrl]
+		  ,[ImageUrl]
+		  ,[IsPublished]
+		  ,tv.[DateCreated]
+		  ,tv.[DateModified]
+		  ,[IsDeleted]
+		  ,tv.[CreatedBy]
+		  ,tv.[ModifiedBy]
+		  ,TotalCount = COUNT(1) OVER()
+	FROM [dbo].[TrainingVideos] as tv
+		inner join dbo.Seasons as s 
+		on s.Id = tv.SeasonId
+		inner join dbo.VideoCategories as vc
+		on vc.Id = tv.CategoryId
+		inner join dbo.Conferences as c
+		on c.Id = tv.ConferenceId
+	Where tv.SeasonId = @SeasonId
+		and tv.ConferenceId = @ConferenceId
+	Order By tv.SeasonId
+	OFFSET @offset ROWS
+	FETCH NEXT @PageSize ROWS ONLY;
+
+END
+
