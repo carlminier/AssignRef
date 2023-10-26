3a2c91bf8ff59cf3c0a54912c4fd59f25ba8f0d0
Carl Minier <carl.a.minier@gmail.com>
2023-10-26 10:43:20 -0700

    Update TrainingVideo.cs
    Cleaning up code

--- Videos_Backend/TrainingVideo.cs
+++ Videos_Backend/TrainingVideo.cs
@@ -1,16 +1,4 @@
-﻿using Sabio.Models.Domain.Conferences;
-using Sabio.Models.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Sabio.Models.Domain.Seasons;
-using Sabio.Models.Domain.Videos;
-
-namespace Sabio.Models.Domain.TrainingVideos
-{
-    public class TrainingVideo : TrainingVideoBase
+public class TrainingVideo : TrainingVideoBase
     {
         public LookUp Category { get; set; }
         public BaseSeason Season { get; set; }
