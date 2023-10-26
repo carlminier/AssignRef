f47166cf82c9af490ae85b144cebc0567eb813f4
Carl Minier <carl.a.minier@gmail.com>
2023-10-26 10:43:32 -0700

    Update TrainingVideoBase.cs
    Cleaning up code

--- Videos_Backend/TrainingVideoBase.cs
+++ Videos_Backend/TrainingVideoBase.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Permissions;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sabio.Models.Domain.TrainingVideos
-{
-    public class TrainingVideoBase
+public class TrainingVideoBase
     {
         public int Id { get; set; }
         public string Title { get; set; }
